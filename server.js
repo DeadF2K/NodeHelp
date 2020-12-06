5b7da2d1bc3541cc123c8c2a71d20a3087640295
rosinetiani <rosine.tiani@parcaune.com>
2020-12-06 16:18:53 +0100

    Füge Admin-Seite hinzu

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get("/logout", (req, res) => {
 
 //cookie monster
 app.get("/admin-login", (req, res) => {
-    if(req.session.isLoggedIn){
+    if(req.session.isLoggedIn && req.session.userRole === "admin"){
         console.log(req.session.userRole);
         res.sendFile(__dirname + '/public/admin-login.html')
     } else {
@@ -46,7 +46,13 @@ app.get("/admin-login", (req, res) => {
 });
 
 app.get("/user-login", (req, res) => {
-
+    if(req.session.isLoggedIn && req.session.userRole === "user"){
+        console.log(req.session.userRole);
+        res.sendFile(__dirname + '/public/user-login.html')
+    } else {
+        req.session = null;
+        res.redirect("/");
+    }
 });
 
 app.get("/info", (req, res) => {
@@ -65,13 +71,13 @@ app.post("/login", (req, res) => {
             if(docs.length === 1){
                 const userRole =  docs[0].role;
                 bcrypt.compare(pw, docs[0].password, (err, result) => {
-                    if(result){ 
+                    if(result){                                                 //logged in?
                         req.session.userRole = userRole;
                         req.session.isLoggedIn = true;
                         if(userRole === "admin") {
-                            res.json({suc:true, redirect:"admin"})
+                            res.json({suc:true, redirect:"admin"})              //login to admin page
                         } else {
-                            res.json({suc:true, redirect:"user"})
+                            res.json({suc:true, redirect:"user"})               //login to user page
                         }
                     } else { res.json({suc:false}); }
                 })
@@ -80,16 +86,81 @@ app.post("/login", (req, res) => {
     } else { res.json({suc:false}); }
 });
 
-/*first user Setup in db
-app.get("/newuser", (req, res) => {
-    const db = new Datastore("db/users.db");
-    db.loadDatabase();
-    bcrypt.hash("admin", 10, (err, hash) => {
-        console.log(err);
-        console.log(hash);
-        db.insert({email: "admin@email.fu", username: "admin", password: hash, role: "admin"}, (err, doc) => {console.log(doc)})
-    });
-})*/
+/*create new users*/
+app.post("/newuser", (req, res) => {
+    if(req.session.isLoggedIn && req.session.userRole === "admin"){
+        console.log(req.body);
+        const nun = req.body.username;
+        const nemail = req.body.email;
+        const db = new Datastore("db/users.db");
+        db.loadDatabase();
+        db.find({username:nun}, (err, docs) => {
+            if(docs.length === 0){
+                db.find({email:nemail}, (err, docs) => {
+                    if(docs.length === 0){
+                        bcrypt.hash(nun, 10, (err, hash) => {
+                            console.log(err);
+                            console.log(hash);
+                            db.insert({email: nemail, username: nun, password: hash, role: "user", suspended: false}, (err, doc) => {
+                                res.json({suc:true});
+                            });
+                        });
+                    } else {
+                        res.json({suc:false});
+                    }            
+                });
+            } else {
+                res.json({suc:false});
+            }   
+        });
+    } else {
+        res.json({suc:false});
+    }
+})
+
+app.get("/getusers", (req, res) => {
+    if(req.session.isLoggedIn && req.session.userRole === "admin"){
+        const db = new Datastore("db/users.db");
+        db.loadDatabase();
+        db.find({role:"user"}, (err, docs) => {
+            if(docs.length > 0) {
+                let resArray = [];
+                docs.forEach(element => {
+                    resArray.push({
+                        username:element.username, 
+                        suspended:element.suspended
+                    })
+                });
+                res.json({suc:true, users:resArray})
+            } else {
+                res.json({suc:false});
+            }
+
+        })
+    } else {
+        res.json({suc:false});
+    }
+});
+
+app.post("/toggleSus", (req, res) => {
+    if(req.session.isLoggedIn && req.session.userRole === "admin"){
+        const db = new Datastore("db/users.db");
+        db.loadDatabase();
+        db.find({username:req.body.username}, (err, docs) => { 
+            if(docs.length === 1) {
+                var now = !docs[0].suspended;
+                db.update({_id: docs[0]._id}, {$set: {suspended: now}}, (err, num) =>{
+                    res.json({suc:true});  
+                })
+            } else {
+                res.json({suc:false});
+            }
+
+        })
+    } else {
+        res.json({suc:false});
+    }
+});
 
 app.listen(8080, () => {
     console.log("Sever online on port 8080.");
